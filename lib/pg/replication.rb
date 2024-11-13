94f85759346b1e1a288e596a2a830a9b61b9796e
Rodrigo Navarro <rnavarro1@gmail.com>
2024-11-13 17:53:51 -0300

    Generalize to get a replication stream of any output plugin.

--- lib/pg/replication.rb
+++ lib/pg/replication.rb
@@ -8,30 +8,23 @@ require_relative "replication/stream"
 
 module PG
   module Replication
-    def start_pgoutput_replication_slot(slot, publications, location: "0/0", keep_alive_secs: 10)
-      publications = Array(publications)
-        .map { |name| quote_ident(name) }
-        .map { |name| "'#{name}'" }
-        .join(",")
-
+    def start_replication_slot(slot, logical: true, location: "0/0", keep_alive_secs: 10, **params)
       query(<<~SQL)
         START_REPLICATION SLOT
-        #{slot} LOGICAL #{location}
-        ("proto_version" '1', "publication_names" #{publications})
+        #{slot} #{logical ? "LOGICAL" : "PHYSICAL"} #{location}
+        (#{params.map { |k, v| "#{quote_ident(k.to_s)} '#{v}'" }.join(", ")})
       SQL
 
       last_keep_alive = Time.now
       last_processed_lsn = 0
 
       stream = Stream.new(self)
-      stream.lazy.map do |msg|
+      stream.lazy.filter_map do |msg|
         case msg
         in Protocol::XLogData(lsn:, data:)
-          buffer = Buffer.from_string(data.force_encoding(internal_encoding))
-          PGOutput.read_message(buffer).tap do
-            last_processed_lsn = lsn
-            stream.standby_status_update(write_lsn: last_processed_lsn)
-          end
+          last_processed_lsn = lsn
+          stream.standby_status_update(write_lsn: last_processed_lsn)
+          data
 
         in Protocol::PrimaryKeepalive(server_time:, asap: true)
           stream.standby_status_update(write_lsn: last_processed_lsn)
@@ -46,6 +39,14 @@ module PG
           end
           next
         end
+      end
+    end
+
+    def start_pgoutput_replication_slot(slot, publication_names, **kwargs)
+      publication_names = publication_names.join(",")
+      start_replication_slot(slot, **kwargs.merge(proto_version: "1", publication_names:)).map do |data|
+        buffer = Buffer.from_string(data.force_encoding(internal_encoding))
+        PGOutput.read_message(buffer)
       end
     end
   end
