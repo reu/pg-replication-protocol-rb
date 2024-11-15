48c56cb21baa7bae41d2a24cfc3d9f56a53143e8
Rodrigo Navarro <rnavarro1@gmail.com>
2024-11-15 11:50:38 -0300

    Fix keepalive

--- lib/pg/replication.rb
+++ lib/pg/replication.rb
@@ -4,50 +4,76 @@ require "pg"
 require_relative "replication/version"
 require_relative "replication/buffer"
 require_relative "replication/pg_output"
-require_relative "replication/stream"
+require_relative "replication/protocol"
 
 module PG
   module Replication
-    def start_replication_slot(slot, logical: true, location: "0/0", keep_alive_secs: 10, **params)
+    def start_replication_slot(slot, logical: true, location: "0/0", **params)
+      keep_alive_secs = query(<<~SQL).getvalue(0, 0)&.to_i || 10
+        SELECT setting FROM pg_catalog.pg_settings WHERE name = 'wal_receiver_status_interval'
+      SQL
+
       query(<<~SQL)
         START_REPLICATION SLOT
         #{slot} #{logical ? "LOGICAL" : "PHYSICAL"} #{location}
-        (#{params.map { |k, v| "#{quote_ident(k.to_s)} '#{v}'" }.join(", ")})
+        (#{params.map { |k, v| "#{quote_ident(k.to_s)} '#{escape_string(v.to_s)}'" }.join(", ")})
       SQL
 
-      last_keep_alive = Time.now
       last_processed_lsn = 0
+      last_keep_alive = Time.now
 
-      Stream.new(self).lazy.filter_map do |msg|
-        case msg
-        in Protocol::XLogData(lsn:, data:)
-          last_processed_lsn = lsn
-          standby_status_update(write_lsn: last_processed_lsn)
-          last_keep_alive = Time.now
-          data
+      Enumerator
+        .new do |y|
+          loop do
+            if Time.now - last_keep_alive > keep_alive_secs
+              standby_status_update(write_lsn: last_processed_lsn)
+              last_keep_alive = Time.now
+            end
 
-        in Protocol::PrimaryKeepalive(server_time:, asap: true)
-          standby_status_update(write_lsn: last_processed_lsn)
-          last_keep_alive = Time.now
-          next
+            consume_input
+            next if is_busy
 
-        in Protocol::PrimaryKeepalive(server_time:)
-          now = Time.now
-          if now - last_keep_alive > keep_alive_secs
-            standby_status_update(write_lsn: last_processed_lsn)
-            last_keep_alive = now
+            case get_copy_data(async: true)
+            in nil
+              get_last_result
+              break
+
+            in false
+              IO.select([socket_io], nil, nil, keep_alive_secs)
+              next
+
+            in data
+              buffer = Buffer.new(StringIO.new(data))
+              y << Protocol.read_message(buffer)
+            end
           end
-          next
         end
-      end
+        .lazy
+        .filter_map do |msg|
+          case msg
+          in Protocol::XLogData(lsn:, data:)
+            last_processed_lsn = lsn
+            standby_status_update(write_lsn: last_processed_lsn)
+            last_keep_alive = Time.now
+            data
+
+          in Protocol::PrimaryKeepalive(server_time:, asap: true)
+            standby_status_update(write_lsn: last_processed_lsn)
+            last_keep_alive = Time.now
+            next
+
+          else
+            next
+          end
+        end
     end
 
     def start_pgoutput_replication_slot(slot, publication_names, **kwargs)
       publication_names = publication_names.join(",")
-      start_replication_slot(slot, **kwargs.merge(proto_version: "1", publication_names:)).map do |data|
-        buffer = Buffer.from_string(data.force_encoding(internal_encoding))
-        PGOutput.read_message(buffer)
-      end
+
+      start_replication_slot(slot, **kwargs.merge(proto_version: "1", publication_names:))
+        .map { |data| data.force_encoding(internal_encoding) }
+        .map { |data| PGOutput.read_message(Buffer.from_string(data)) }
     end
 
     def standby_status_update(
